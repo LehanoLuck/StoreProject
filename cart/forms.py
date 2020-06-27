702b0bfc6088f7c8979f829f351d9ae4b9a056dd
niconiners <aleksej.sirotin.1999@mail.ru>
2020-06-27 17:42:45 +0700

    Обновил страницу с товаром

--- cart/forms.py
+++ cart/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 
-PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 11)]
+PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 6)]
 
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
+
+    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int, widget=forms.RadioSelect())
     update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
